7887c656d5c406246e6e1669be618b4e9fe690be
venkat <venkat@gmail.com>
2025-02-03 15:40:43 +0530

    modified

---
+++ functions.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+  echo "installation of $2 failed"
+  exit 1
+else
+  echo "installation of $2 success"
+fi  
+     
+
+}
+
+if [ $ID -ne 0 ]
+then
+   echo "you are not a root user"
+   exit 1
+
+else
+   echo " you are a root user"
+fi
+
+yum install net-tools -y
+
+VALIDATE $? "installing net-tools"
+
+yum install git -y
+
+VALIDATE $? "installing git"
+     
+
