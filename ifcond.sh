e83a4ce7df1e2c69ce1295a3eded7693ab689c5a
venkat <venkat@gmail.com>
2025-02-03 14:37:46 +0530

    modified

--- ifcond.sh
+++ ifcond.sh
@@ -5,6 +5,19 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
  echo " ERROR: please run with root user"
+ exit 1
 else
  echo "you are a root user"
 fi
+
+yum install mysql -y
+
+EXITCODE=$($?)
+
+if [ EXITCODE -ne 0 ]
+then
+   echo "error: mysql is not installed"
+   exit 1 
+else
+   echo " mysql installed successfully "
+fi     
