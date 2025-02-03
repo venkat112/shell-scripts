58860bb8a789e82836be3f5f64ccdd52d22e14f8
venkat <venkat@gmail.com>
2025-02-03 14:16:49 +0530

    modified

--- ifcond.sh
+++ ifcond.sh
@@ -4,7 +4,7 @@ ID=$(id -u)
 
 if [ $ID -ne 0]
 then
-echo " ERROR: please run with root user"
+ echo " ERROR: please run with root user"
 else
-echo "you are a root user"
+ echo "you are a root user"
 fi
